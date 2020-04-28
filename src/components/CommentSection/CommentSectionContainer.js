efac6caea594b00f416f220fc0645445f5f9d331
unknown <guivecchi12@gmail.com>
2020-04-28 11:47:09 -0600

    finished

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -14,8 +14,8 @@ const CommentSection = props => {
     <div>
       {/* map through the comments data and return the Comment component */}
       {
-        data.map((element)=>{
-          return (<Comment comment={element}/>);
+        data.map((comment)=>{
+          return (<Comment comment={comment}/>);
         })
       }
       <CommentInput />
